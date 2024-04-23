ce662ed580ee10b104d97d6bbe2d39aef186a06d
Rodrigo Reis <suporteproc@alema.local>
2024-04-23 15:42:29 -0400

    feat: procurando os valores no google shopping

--- bot.py
+++ bot.py
@@ -14,7 +14,40 @@ from botcity.web.browsers.chrome import default_options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+import pandas as pd
+
 class Bot(WebBot):
+
+    produtos = pd.read_excel('buscas.xlsx')
+
+    def search_google_shopping(self, product, min_value, max_value):
+
+        self.find_element('//*[@id="APjFqb"]', By.XPATH).send_keys(str(product)) 
+        self.wait(1000)
+        self.enter()
+
+        menus_google = self.find_element('crJ18e', By.CLASS_NAME)
+        
+        if menus_google.is_displayed(): 
+            shopping_link = menus_google.find_element(By.LINK_TEXT, 'Shopping')
+            if shopping_link:
+                shopping_link.click()
+
+        self.wait(2000)
+    
+
+        price_product =  self.find_elements('kHxwFf', By.CLASS_NAME)
+
+        for price in price_product:
+            print(price.text)
+    
+    
+    
+    def search_buscape_shopping(self):
+        pass
+
+    def send_email():
+        pass
 
     def action(self, execution):
         # Runner passes the server url, the id of the task being executed,
@@ -37,10 +70,14 @@ class Bot(WebBot):
         self.driver_path = ChromeDriverManager().install()
 
         # Opens the BotCity website.
-        self.browse("https://www.botcity.dev")
+        self.browse("https://google.com.br")
+
+        # Maximize Window
+        self.maximize_window()
 
         # Implement here your logic...
-        ...
+        
+        self.search_google_shopping('iphone 11', '2000', '3000')
 
         # Wait 3 seconds before closing
         self.wait(3000)
@@ -51,11 +88,11 @@ class Bot(WebBot):
         self.stop_browser()
 
         # Uncomment to mark this task as finished on BotMaestro
-        # maestro.finish_task(
-        #     task_id=execution.task_id,
-        #     status=AutomationTaskFinishStatus.SUCCESS,
-        #     message="Task Finished OK."
-        # )
+        maestro.finish_task(
+            task_id=execution.task_id,
+            status=AutomationTaskFinishStatus.SUCCESS,
+            message="Task Finished OK."
+        )
 
 
     def not_found(label):
